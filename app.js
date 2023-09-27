0b92a82ea1319782d4902bff5265f9341bb1958e
papai004 <singhapapai004@gmail.com>
2023-09-28 00:31:22 +0530

    updated route, controller and service of school api

--- app.js
+++ app.js
@@ -7,33 +7,30 @@ import helmet from "helmet";
 
 dotenv.config();
 
-// * Local imports 
+// * Local imports
 import schoolRoutes from "./src/routes/school.js";
-import getSchoolRoutes from "./src/routes/getSchool.js";
-import deleteSchoolRoutes from "./src/routes/deleteSchool.js";
-import updateSchoolRoutes from "./src/routes/updateSchool.js"
 import globalErrorHandler from "./src/middlewares/globalErrorHandler.js";
 
 // * The express app
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
 
 // * Connecting to the DB
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
-  console.log("Connected to DB successfully!!")
-}).catch((err) => {
-  console.error("DB connection failed", err);
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to DB successfully!!");
+  })
+  .catch((err) => {
+    console.error("DB connection failed", err);
+  });
 
 // * School routes
 app.use("/school", schoolRoutes);
-app.use("/school", getSchoolRoutes);
-app.use("/school", deleteSchoolRoutes);
-app.use("/school",updateSchoolRoutes);
 
 app.get("/", function (req, res) {
   res.send("Hello World");
--- src/services/school.service.js
+++ src/services/school.service.js
@@ -2,7 +2,6 @@ import bcrpt from "bcrypt";
 
 import SchoolModel from "../models/school.model.js";
 
-
 class SchoolService {
   constructor() {}
   async createNewSchool(payload) {
@@ -61,6 +60,107 @@ class SchoolService {
         }
       } catch (err) {
         reject(err);
+      }
+    });
+  }
+
+  async getSchoolService(payload) {
+    return new Promise(async (resolve, reject) => {
+      const { email } = payload;
+      try {
+        // * Checking if the school is available or not
+        const isSchoolFound = await SchoolModel.findOne({ email });
+        if (!isSchoolFound) {
+          reject(
+            new Error("This School is not found", {
+              cause: { indicator: "db", status: 400 },
+            })
+          );
+          return;
+        }
+
+        // * If the School is found we will simply resolve it
+        resolve(isSchoolFound);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  async updateSchoolService(payload) {
+    return new Promise(async (resolve, reject) => {
+      const { username, schoolName, highestGrade, lowestGrade, email } =
+        payload;
+
+      try {
+        const findSchool = await SchoolModel.findOne({ email });
+        console.log("The school is: ", findSchool);
+
+        if (!findSchool) {
+          reject(
+            new Error("This School is not found", {
+              cause: { indicator: "db", status: 400 },
+            })
+          );
+          return;
+        } else {
+          // Now updating all the payload values
+          findSchool.username = username;
+          findSchool.email = email;
+          findSchool.schoolName = schoolName;
+          findSchool.highestGrade = highestGrade;
+          findSchool.lowestGrade = lowestGrade;
+          const setIsUpdated = await findSchool.save();
+
+          resolve(setIsUpdated);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  async deleteSchoolService(payload) {
+    return new Promise(async (resolve, reject) => {
+      const { email, password } = payload;
+
+      try {
+        const findSchool = await SchoolModel.findOne({ email });
+
+        if (!findSchool || findSchool.isDeleted === true) {
+          reject(
+            new Error("This School is not found", {
+              cause: { indicator: "db", status: 400 },
+            })
+          );
+          return;
+        } else {
+          /**
+           * IF the school is present then hashing the password and matching
+           * with the password
+           */
+          const isCorrectPassword = await bcrpt.compare(
+            password,
+            findSchool.password
+          );
+
+          if (isCorrectPassword) {
+            console.log("Password is correct");
+            // Now set isDeleted to true of the School if the password is correct.
+            findSchool.isDeleted = true;
+            const setIsUpdated = await findSchool.save();
+            resolve(setIsUpdated);
+          } else {
+            //* Password does not match
+            reject(
+              new Error("Incorrect Password", {
+                cause: { indicator: "Wrong Password", status: 404 },
+              })
+            );
+          }
+        }
+      } catch (error) {
+        reject(error);
       }
     });
   }
