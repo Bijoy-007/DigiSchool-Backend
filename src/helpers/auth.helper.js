14654a1d7f73ee3714db242e80b93bf417b478de
Bijoy Das <dasbijoy9804417767@gmail.com>
2023-10-07 13:35:13 +0530

    Added authentication

--- src/helpers/auth.helper.js
+++ src/helpers/auth.helper.js
@@ -66,6 +66,78 @@ class AuthHelper {
       }
     });
   }
+
+  async validateRefreshToken(refreshToken, ipAddress) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        /**
+         ** Decoding the refresh token first. If the refresh token is valid then
+         ** it will contain the schoolId inside the {id} key
+         ** The syntax {id: schoolId} = ..... just means I am destructuring an object and taking
+         ** the {id} key and re-naming its as {schoolId}
+         */
+        const { id: schoolId } = jwt.verify(
+          refreshToken,
+          process.env.REFRESH_TOKEN_SECRET_KEY
+        );
+
+        // * Finding the refresh token in the DB
+        const foundRefreshToken = await RefreshToken.findOne({
+          token: refreshToken,
+          // * The shoolId should be the same the encrypted schoolId
+          schoolId: schoolId,
+          /**
+           ** The request to refresh the access token should come from the same IP address
+           ** to which which this refresh token was being assigned to. Hence matching IP
+           */
+          ipAddress,
+          // * When the user logs out then the refresh token will be marked as DELETED
+          isDeleted: false,
+        });
+
+        /**
+         ** If we get a refresh token in the DB which fulfills all the conditions
+         ** that means that refresh token is valid and we should return validated
+         */
+
+        if (foundRefreshToken) {
+          resolve({
+            isValidated: true,
+            schoolId,
+          });
+        } else {
+          /**
+           ** You might think that status should be Not Found and a message according to that BUT
+           ** please note the client should only that the token is in-valid there is no need to
+           ** expore the internal implemenation of our security system . If the refresh token is not
+           ** present in the DB that's not our fault so we should not disclose that.
+           */
+          reject(
+            new Error("Invalid refresh token. Can't create a access token.", {
+              cause: { indicator: "auth", status: 401 },
+            })
+          );
+        }
+      } catch (err) {
+        /**
+         ** Handling errors in case if JWT verification fails. If JWT verification fails then
+         ** the error will be TokenExpiredError or JsonWebTokenError or NotBeforeError
+         */
+        if (
+          err.name === "TokenExpiredError" ||
+          err.name === "JsonWebTokenError" ||
+          err.name === "NotBeforeError"
+        ) {
+          reject(
+            new Error(err.message, {
+              cause: { indicator: "auth", status: 401, details: err },
+            })
+          );
+        }
+        reject(err);
+      }
+    });
+  }
 }
 
 export default Object.freeze(new AuthHelper());
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import AuthHelper from "../helpers/auth.helper.js";
 import schoolModel from "../models/school.model.js";
@@ -27,15 +28,66 @@ class AuthService {
 
         // * If the password is wrong
         if (!isCorrect) {
-          throw new Error("Incorrect password. Please provide the correct password!", {
-            cause: { indicator: "auth", status: 401 },
-          });
+          throw new Error(
+            "Incorrect password. Please provide the correct password!",
+            {
+              cause: { indicator: "auth", status: 401 },
+            }
+          );
         }
 
         const { accessToken, refreshToken } = await AuthHelper.generateTokens(
           foundSchool._id,
           ipAddress
         );
+
+        // * If everyting was successful
+        resolve({
+          accessToken,
+          refreshToken,
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  /**
+   * This function is responsible for generating access-token using refresh token
+   * @param {*} refrshToken
+   * @param {*} ipAddress
+   * @returns {*} {accessToken: string}
+   */
+  async refreshAccessToken(refreshToken, ipAddress) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // * Validating the refresh token
+        const { isValidated, schoolId } = await AuthHelper.validateRefreshToken(
+          refreshToken,
+          ipAddress
+        );
+
+        if (isValidated) {
+          // If the refresh token is validated successfully then creating a new access token
+          const accessToken = await jwt.sign(
+            { id: schoolId },
+            process.env.ACCESS_TOKEN_SECRET_KEY,
+            {
+              /**
+               * 1 Seceond = 1000 Mili seconds
+               * So 10 Minutes = 10 X 60 seconds = 10 X 60 X 1000 mili seconds
+               */
+              expiresIn: 10 * 60 * 100,
+            }
+          );
+          resolve({ accessToken });
+        } else {
+          reject(
+            new Error("Invalid refresh token. Can't create a access token.", {
+              cause: { indicator: "auth", status: 401 },
+            })
+          );
+        }
 
         // * If everyting was successful
         resolve({
